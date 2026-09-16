83b738c975a1d1fccfc35d266c3fe9f0d8bbf6e6
Maria <northallien@gmail.com>
2026-09-16 14:58:28 +0300

    Добавлена главная страница сайта

--- vitahub/vitahub/settings.py
+++ vitahub/vitahub/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b6ir#7^(uer!xqhwi6q@g4zv3=r00%od99v^**mo1bp#-tq%1m'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
